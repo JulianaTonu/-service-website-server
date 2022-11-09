f5c5e8dcd1902f0097fb2add78ad8c04be2b754b
unknown <julianatonu@gmail.com>
2022-11-09 20:49:43 +0600

    added JWT and accesstoken

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require("express")
 const cors =require("cors")
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt =require('jsonwebtoken')
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -12,7 +13,7 @@ console.log('user',process.env.DB_USER)
 console.log('password',process.env.DB_PASSWORD)
 
 
-
+//mongodb
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rpina.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -105,6 +106,14 @@ app.get('/services/:id', async(req, res)=>{
     const query ={_id:ObjectId(id)};
     const service =await serviceCollection.findOne(query)
     res.send(service);
+})
+
+//delete reviews
+app.delete('/reviews/:id', async(req,res)=>{
+    const id =req.params.id;
+    const query ={_id:ObjectId(id)}
+    const result =await reviewCollection.deleteOne(query)
+    res.send(result)
 })
 
 }
